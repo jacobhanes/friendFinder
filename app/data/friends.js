1a1298af9ea7a2cc9b3ce0a90f511939839e5b03
jake hanes <jhanesx@yahoo.com>
2019-08-25 16:33:37 -0700

    home page made small amount of friends are made

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+const friends = [
+    {
+        Name: "Jake",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjP3MeslZ_kAhVOJjQIHSqGCp0QjRx6BAgBEAQ&url=https%3A%2F%2Fstrategywiki.org%2Fwiki%2FSuper_Smash_Bros._Melee%2FCaptain_Falcon&psig=AOvVaw01NWpav-XOaskrH8EYAo2O&ust=1566861834385336",
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    },
+    {
+        
+    }
+]
