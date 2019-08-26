a3b6908b0b69159a14e63610aeacffb5a00644af
jake hanes <jhanesx@yahoo.com>
2019-08-25 17:26:05 -0700

    friends made

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,6 +5,36 @@ const friends = [
         scores: [5,5,5,5,5,5,5,5,5,5]
     },
     {
-        
-    }
+        Name: "Tommy Boy",
+        photo: "https://filmschoolrejects.com/wp-content/uploads/2017/04/03IWPVYyhyq2xgY3X-700x400.jpg",
+        scores: [5,4,3,1,2,2,3,4,2,1]
+    },
+    {
+        Name: "Steve Nash",
+        photo: "https://www.coastmountainnews.com/wp-content/uploads/2018/03/11235020_web1_Steve-Nash_Flickr.jpg",
+        scores: [5,2,5,3,5,1,3,3,2,3]
+    },
+    {
+        Name: "Juggernaut",
+        photo: "https://gamepedia.cursecdn.com/dota2_gamepedia/0/03/Juggernaut_icon.png",
+        scores: [5,4,5,5,3,5,1,5,3,5]
+    },
+    {
+        Name: "Donna",
+        photo: "https://i.pinimg.com/originals/02/bc/44/02bc44240980800d9c8e64a258d3798f.jpg",
+        scores: [1,1,1,1,1,5,5,5,5,5]
+    },
+    {
+        Name: "Nancy",
+        photo: "https://image.freepik.com/free-photo/upset-woman-looking-camera_23-2147808180.jpg",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        Name: "Lycan",
+        photo: "https://gamepedia.cursecdn.com/dota2_gamepedia/d/d6/Lycan_icon.png",
+        scores: [5,5,5,3,2,5,2,1,3,3]
+    },
+   
 ]
+
+module.exports = friends;
